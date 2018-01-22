53f4ef6dbb87677f8fae54ef7b174b1a1efc6857
Maverick Tesini <mavericktesini@gmail.com>
2018-01-22 19:26:28 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,7 +2,4 @@ var playlist = {singerA: "Song A"}
 
 function updatePlaylist(playlist, artistName, songTitle) {
   // return playlist.songTitle = artistName;
-  var placeholder = songTitle;
-  playlist[placeholder] = artistName;
-  return playlist['artistName']
 }
